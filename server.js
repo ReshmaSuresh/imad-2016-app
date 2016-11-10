2cc81d4098dd5096eee4a3fd9904b93102506b40
Reshma Suresh <reshmasuresh3009@gmail.com>
2016-11-11 01:25:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'IMG_0915.JPG'));
+  res.sendFile(path.join(__dirname, 'ui', '10954684_1184277164919250_247758682_n.jpg'));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
