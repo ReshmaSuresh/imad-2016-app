9af4eac553e07e178a66cf7f16e02449357189a0
Reshma Suresh <reshmasuresh3009@gmail.com>
2016-11-12 01:19:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,12 +95,6 @@ app.get('/:blogNo', function(req, res) {
     var blogNo= req.params.blogNo;
     res.send(createTemplate(blogs[blogNo]));
 });
-app.get('/article-two', function(req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three', function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
