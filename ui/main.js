c8789ade8a453a38688257f94fd4ae9bd331f25e
Reshma Suresh <reshmasuresh3009@gmail.com>
2016-11-12 18:42:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
-console.log('Loaded!');
+var button=document.getElementById("likebutton");
+var likebutton=0;
+button.onclick= function(){
+    likebutton=likebutton+1;
+    var span=document.getElementById("likes");
+    span.innerHTML=likebutton.toString();
+};
