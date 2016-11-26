d41a278f637a3aff0a6c77ed76b487e14d154486
Reshma Suresh <reshmasuresh3009@gmail.com>
2016-11-26 18:26:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 var config = {
   host: 'db.imad.hasura-app.io',
   port:'5432',
@@ -13,6 +14,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
  function createTemplate (data){
      var title= data.title;
      var heading= data.heading;
@@ -77,17 +79,37 @@ app.get('/counter', function (req,res) {
    counter=counter+1;
    res.send(counter.toString());
 });
+
+app.post('create-user/', function(req,res){
+    var username= req.body.username;
+    var password=req.body.password;
+    var salt = crypto.getRandomBytes(128).toString('hex');
+    var dbString = hash(password , salt);
+    pool.query('INSERT INTO "user" (username, password) VALUES ($1,$2)', [username,dbString], function(err, result){
+         if(err){
+            res.status(500).send(err.toString());
+        } 
+        else {
+            res.send("User Successfully Created" +username);
+        }
+        
+    });
+});
+
 app.get('/hash/:input', function(req,res){
     var hashedString = hash(req.params.input,'thiisisis-random');
     res.send(hashedString);
 });
+
 function hash(input,salt) {
      var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
      return hashed.toString('hex');
 }
+
 app.get('/about', function(req,res) {
     res.sendFile(path.join(__dirname, 'ui', 'about.html'));
 });
+
 app.get('/blogs/:blogNo', function(req, res) {
    // var blogNo= req.params.blogNo;
     pool.query("SELECT * FROM blogs WHERE title = $1" , [req.params.blogNo] , function(err,result) {
@@ -108,12 +130,15 @@ app.get('/blogs/:blogNo', function(req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
