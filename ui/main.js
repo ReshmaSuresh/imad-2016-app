66d6e43e49b805ca935a70248faace5af7741194
Reshma Suresh <reshmasuresh3009@gmail.com>
2016-11-26 20:36:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,17 @@
    var button=document.getElementById('likebutton');
-    var likebutton = 0;
     button.onclick= function() {
-        likebutton=likebutton + 1;
-        var span=document.getElementById('likes');
-        span.innerHTML = likebutton.toString();
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+            if(request.readyState===XMLHttpRequest.DONE){
+                if(request.status === 200){
+                   var likebutton= request.responseText;
+                   var span=document.getElementById('likes');
+                   span.innerHTML = likebutton.toString();
+                }
+            }
+            
+        };
+       request.open('GET','http://reshmasuresh.imad.hasura-app.io/counter',true);
+       request.send(null);
+        
     };
