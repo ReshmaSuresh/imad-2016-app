52ec0cbc3594df3cc4b8f780f2ad9b95780b599b
Reshma Suresh <reshmasuresh3009@gmail.com>
2016-11-26 22:41:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,3 +40,36 @@ submit.onclick= function(){
        request.send(null);
   
 };
+
+
+
+
+
+
+
+
+
+/*var submit = document.getElementById('submit');
+submit.onclick= function(){
+    var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+            if(request.readyState===XMLHttpRequest.DONE){
+                if(request.status === 200){
+                    var comments = request.responseText;
+                    comments = JSON.parse(comments);
+                    var list = '';
+                    for(var i=0;i<comments.length;i++) {
+                        list += '<li>' + comments[i] + '</li>';
+                    }
+                    var ul =  document.getElementById('commentslist');
+                    ul.innerHTML=list;
+                }
+            }
+            
+        };
+       var commentInput = document.getElementById('comment');
+       var comment = commentInput.value;
+       request.open('POST','http://reshmasuresh.imad.hasura-app.io/submit-comment?comment=' + comment,true);
+       request.send(null);
+  
+};
