f1c42609d7ad580cf5b2cb54233ce90f0f59c833
Reshma Suresh <reshmasuresh3009@gmail.com>
2016-11-26 21:54:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,11 +19,22 @@ var commentInput = document.getElementById('comment');
 var comment = commentInput.value;
 var submit = document.getElementById('submit');
 submit.onclick= function(){
-    var comments = ['nice', 'good','okay'];
-    var list = '';
-    for(var i=0;i<comments.length;i++) {
-        list += '<li>' + comments[i] + '</li>';
-    }
-    var ul =  document.getElementById('commentslist');
-    ul.innerHTML=list;
+    var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+            if(request.readyState===XMLHttpRequest.DONE){
+                if(request.status === 200){
+                    var comments = ['nice', 'good','okay'];
+                    var list = '';
+                    for(var i=0;i<comments.length;i++) {
+                        list += '<li>' + comments[i] + '</li>';
+                    }
+                    var ul =  document.getElementById('commentslist');
+                    ul.innerHTML=list;
+                }
+            }
+            
+        };
+       request.open('GET','http://reshmasuresh.imad.hasura-app.io/counter',true);
+       request.send(null);
+  
 };
